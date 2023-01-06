7596f5e0a3cc0115d7ce87c4d6a930f52664e8a0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2023-01-06 23:54:06 +0000

    set prod api url

---
+++ env-config.js
@@ -0,0 +1,5 @@
+const prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  'process.env.BACKEND_URL': prod ? 'https://sheltered-chamber-10958.herokuapp.com/api/shows' : 'http://localhost:3020/api/shows'
+}
